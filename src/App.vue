ea5260ff1a295c7436da317f37cc989a9b389ac8
Cianuro-83 <buzzeosimone@yahoo.com>
2023-03-01 15:59:56 +0100

    FEAT: IMPOSTAZIONE DEL PROGETTO

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<!-- **************************
+JAVASCRIPT
+************************** -->
+
+<script>
+// **************************
+// IMPORT
+// **************************
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+//********************
+// EXPORT
+//******************** */
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
 </script>
 
+<!-- **************************
+HTML
+************************** -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<!-- **************************
+STYLE
+************************** -->
+<style lang="scss">
+@use "./style/general.scss";
 </style>
